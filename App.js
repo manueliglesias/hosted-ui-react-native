e0b16055022a7f97d12cdd648b94c7e09825d751
Manuel Iglesias <manuelig@amazon.com>
2019-02-12 12:40:16 -0800

    Update dependencies

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, Button, StatusBar } from 'react-native';
-import { Constants } from 'expo';
+import { StyleSheet, Text, ScrollView, SafeAreaView, StatusBar, TouchableOpacity, View, Platform } from 'react-native';
+import { Linking, WebBrowser } from 'expo';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { default as Amplify } from "aws-amplify";
 import { withOAuth } from "aws-amplify-react-native";
@@ -8,31 +9,44 @@ import { default as awsConfig } from "./aws-exports";
 
 Amplify.configure(awsConfig);
 
-// Amplify.Logger.LOG_LEVEL = 'DEBUG';
-
 Amplify.configure({
   Auth: {
     oauth: {
       // Domain name
       domain: 'manolo-hosted-ui.auth.us-east-1.amazoncognito.com',
+      // domain: 'mhvhvghj.auth.us-east-1.amazoncognito-gamma.com',
 
       // Authorized scopes
       scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+      // scope: ['email', 'openid'],
 
       // Callback URL
-      redirectSignIn: Constants.linkingUrl,
+      redirectSignIn: Linking.makeUrl('/'),
 
       // Sign out URL
-      redirectSignOut: Constants.linkingUrl,
+      redirectSignOut: Linking.makeUrl('/'),
 
       // 'code' for Authorization code grant, 
       // 'token' for Implicit grant
       responseType: 'code',
+      // responseType: 'token',
 
       // optional, for Cognito hosted ui specified options
       options: {
         // Indicates if the data collection is enabled to support Cognito advanced security features. By default, this flag is set to true.
-        AdvancedSecurityDataCollectionFlag: true
+        AdvancedSecurityDataCollectionFlag: true,
+      },
+
+      urlOpener: async (url, redirectUrl) => {
+        const { type, url: newUrl } = await WebBrowser.openAuthSessionAsync(url, redirectUrl);
+
+        if (type === 'success') {
+          await WebBrowser.dismissBrowser();
+
+          if (Platform.OS === 'ios') {
+            return Linking.openURL(newUrl);
+          }
+        }
       }
     }
   }
@@ -40,26 +54,65 @@ Amplify.configure({
 
 class App extends React.Component {
   render() {
-    const { oAuthUser: user, signIn, signOut } = this.props;
+    const {
+      oAuthUser: user,
+      oAuthError: error,
+      hostedUISignIn,
+      facebookSignIn,
+      googleSignIn,
+      amazonSignIn,
+      customProviderSignIn,
+      signOut
+    } = this.props;
 
     return (
-      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>{JSON.stringify(user, null, 2)}</Text>
-        {user
-          ? <Button title="Sign Out" onPress={signOut} />
-          : <Button title="Sign In" onPress={signIn} />}
-      </ScrollView>
+      <SafeAreaView style={styles.safeArea}>
+        {user && <Button title="Sign Out" onPress={signOut} icon='logout' />}
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+          <Text>{JSON.stringify({ user, error, appUrl: Linking.makeUrl('/') }, null, 2)}</Text>
+          {!user && <React.Fragment>
+            <Button title="Cognito" onPress={hostedUISignIn} icon='account-key' />
+            <Button title="Facebook" onPress={facebookSignIn} icon='facebook-box' />
+            <Button title="Google" onPress={googleSignIn} icon='google' />
+            <Button title="Amazon" onPress={amazonSignIn} icon='amazon' />
+            <Button title="Yahoo" onPress={() => customProviderSignIn('Yahoo')} icon='yahoo' />
+          </React.Fragment>}
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
 
+const Button = ({ onPress = () => { }, title, icon, backgroundColor = '#FF9900', color = 'white' }) => (
+  <TouchableOpacity onPress={onPress} activeOpacity={0.5} style={{
+    backgroundColor,
+    flexDirection: 'column',
+    margin: 5,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  }}>
+    <View style={{
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 5,
+    }}>
+      {icon && <MaterialCommunityIcons name={icon} size={32} color={color} style={{
+        marginRight: title && 5
+      }} />}
+      {title && <Text style={{ color }}>{title}</Text>}
+    </View>
+  </TouchableOpacity>
+);
+
 const styles = StyleSheet.create({
+  safeArea: {
+    flexGrow: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
   scrollViewContainer: {
     flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: StatusBar.currentHeight
   }
 });
 
